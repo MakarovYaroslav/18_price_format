30aafd0db38684e8a796e96cb207a8333c1d5cb9
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-12-17 13:55:38 +0400

    Добавлено тестирование входных данных

--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestInputData(unittest.TestCase):
+
+    def test_letters(self):
+        self.assertFalse(format_price('234f.4245'))
+        self.assertFalse(format_price('234.4g245'))
+        self.assertFalse(format_price('fsff.sfasdf'))
+
+    def test_splitter(self):
+        self.assertFalse(format_price('134/3124'))
+        self.assertFalse(format_price('1343124'))
+        self.assertFalse(format_price('134,3124'))
+        self.assertFalse(format_price('134 3124'))
+        self.assertFalse(format_price('134..3124'))
+        self.assertEqual(format_price('1345.3124'), '1 345 ')
+
+
+if __name__ == '__main__':
+    unittest.main()
