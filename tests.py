48882afe441a81d81863ac6121c8e54fe527d704
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2017-01-07 19:46:15 +0400

    Добавлены новые тесты
    Добавлены тесты расставления пробелов в цене и форматирования цены

--- tests.py
+++ tests.py
@@ -1,21 +1,41 @@
 import unittest
-from format_price import format_price
+from format_price import format_price, add_spaces_around_symbols, test_price
 
 
 class TestInputData(unittest.TestCase):
 
     def test_letters(self):
-        self.assertFalse(format_price('234f.4245'))
-        self.assertFalse(format_price('234.4g245'))
-        self.assertFalse(format_price('fsff.sfasdf'))
+        self.assertFalse(test_price('234f.4245'))
+        self.assertFalse(test_price('234.4g245'))
+        self.assertFalse(test_price('fsff.sfasdf'))
 
     def test_splitter(self):
-        self.assertFalse(format_price('134/3124'))
-        self.assertFalse(format_price('1343124'))
-        self.assertFalse(format_price('134,3124'))
-        self.assertFalse(format_price('134 3124'))
-        self.assertFalse(format_price('134..3124'))
-        self.assertEqual(format_price('1345.3124'), '1 345 ')
+        self.assertFalse(test_price('134/3124'))
+        self.assertFalse(test_price('1343124'))
+        self.assertFalse(test_price('134,3124'))
+        self.assertFalse(test_price('134 3124'))
+        self.assertFalse(test_price('134..3124'))
+        self.assertTrue(test_price('1345.3124'))
+
+
+class TestAddingSpaces(unittest.TestCase):
+
+    def test_spaces(self):
+        self.assertEqual(add_spaces_around_symbols('1241424'), '1 241 424 ')
+        self.assertEqual(add_spaces_around_symbols('13532'), '13 532 ')
+        self.assertEqual(add_spaces_around_symbols('133532'), '133 532 ')
+
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test_format(self):
+        self.assertEqual(format_price('12435.3242'), '12 435 ')
+        self.assertEqual(format_price('155463435.3242'), '155 463 435 ')
+        self.assertIsNone(format_price('12bad2134.34price34'))
+
+    def test_rounding(self):
+        self.assertEqual(format_price('12345.1789'), '12 345 ')
+        self.assertEqual(format_price('12345.6789'), '12 346 ')
 
 
 if __name__ == '__main__':
